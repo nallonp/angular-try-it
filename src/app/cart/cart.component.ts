b5f7258f05ed88a4b984210b6ddeff78ade42131
nallonp <nallonpauluzzi@hotmail.com>
2021-06-04 02:53:11 -0300

    4 files modified, 4 added

---
+++ src/app/cart/cart.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../services/cart.service';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css']
+})
+export class CartComponent {
+  items = this.cartService.getItems();
+
+  constructor(private cartService: CartService) {}
+}
